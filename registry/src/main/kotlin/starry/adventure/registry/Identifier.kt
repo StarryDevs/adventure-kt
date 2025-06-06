7bbc85d94ac6186a0175a10c32aaf34c7eb74e27
WowStarWorld <starworldstudio1@gmail.com>
2025-06-06 21:42:47 +0800

    fix: 未解析的引用 'jvmName'

--- registry/src/main/kotlin/starry/adventure/registry/Identifier.kt
+++ registry/src/main/kotlin/starry/adventure/registry/Identifier.kt
@@ -10,6 +10,7 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlin.reflect.jvm.jvmName
 
 object Identifiers {
 
